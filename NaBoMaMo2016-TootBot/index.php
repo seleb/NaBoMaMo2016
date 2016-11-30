fbe9ceb11528fcaf39bfd455d3cf39e2f9bea4c8
seleb <sean.s.leblanc@gmail.com>
2016-11-30 15:02:06 -0500

    tootbot v2

--- NaBoMaMo2016-TootBot/index.php
+++ NaBoMaMo2016-TootBot/index.php
@@ -2,7 +2,9 @@
 
 
 $statuses=["toot","toot!","TOOT!","TOOT","Toot!","Toot"];
-$MY_STATUS = $statuses[array_rand($statuses)];
+$rare_statuses=["ᴛᴏᴏᴛ","ᵗᵒᵒᵗ","ₜₒₒₜ","﻿ｔｏｏｔ","﻿ＴＯＯＴ","𝓣𝓞𝓞𝓣","𝓽𝓸𝓸𝓽","𝘁𝗼𝗼𝘁","𝗧𝗢𝗢𝗧","toot™"];
+$chosen_statuses=(rand(0,10) < 1 ? $rare_statuses : $statuses);
+$MY_STATUS = $chosen_statuses[array_rand($chosen_statuses)];
 
 if(!isset($MY_CLIENT_ID) || !isset($MY_CLIENT_SECRET)){
 	echo "<h1>Step 1: get client id + secret from /api/v1/apps</h1>";
@@ -92,35 +94,5 @@ echo "<p>";
 var_dump($result);
 echo "</p>";
 $result=json_decode($result, true);
-
-
-
-
-
-
-
-/*echo "<h1>Step 3: use the token to do stuff!</h1>";
-$url = "https://mastodon.social/api/v1/timelines/home";
-$data = array(
-	'response_type' => 'code'
-);
-
-// use key 'http' even if you send the request to https://...
-$options = array(
-    'http' => array(
-        'header'  => "Authorization: Bearer ".$MY_ACCESS_TOKEN."\r\nContent-type: application/x-www-form-urlencoded\r\n",
-        'method'  => 'GET',
-        'content' => http_build_query($data)
-    )
-);
-$context  = stream_context_create($options);
-$result = file_get_contents($url, false, $context);
-if ($result === FALSE) { echo "<p>ERROR</p>"; }
-
-echo "<p>";
-var_dump($result);
-echo "</p>";
-$result=json_decode($result, true);*/
-
 
 ?>
